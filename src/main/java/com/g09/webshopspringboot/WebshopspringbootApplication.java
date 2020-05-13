17ebe1ef38c0cf00d92c3cc691b710ed0d3f5491
Allan Jamil <allanjami_91@hotmail.com>
2020-05-13 10:20:35 +0200

    Initial commit

---
+++ src/main/java/com/g09/webshopspringboot/WebshopspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.g09.webshopspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopspringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/g09/webshopspringboot/WebshopspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g09.webshopspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
