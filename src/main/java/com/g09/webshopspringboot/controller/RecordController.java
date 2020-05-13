e40e5d1a8b30117f0c3abb976cf98b8b47ce481e
Johan Özbek <JohanOzbek@gmail.com>
2020-05-13 12:54:20 +0200

    search, getRecords, getRecord - Backend part finished

--- src/main/java/com/g09/webshopspringboot/controller/RecordController.java
+++ src/main/java/com/g09/webshopspringboot/controller/RecordController.java
@@ -1,10 +1,37 @@
 package com.g09.webshopspringboot.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.g09.webshopspringboot.domain.Record;
+import com.g09.webshopspringboot.service.RecordService;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("records")
 public class RecordController {
+
+    RecordService recordService;
+
+    @Autowired
+    public RecordController(RecordService recordService) {
+        this.recordService = recordService;
+    }
+
+    @GetMapping()
+    public List<Record> getRecords(){
+        return recordService.getRecords();
+    }
+
+    @GetMapping("/{id}")
+    public Record getRecord(@PathVariable Long id){
+        return recordService.findById(id);
+    }
+
+    @GetMapping("/search/{searchWord}")
+    public List<Record> getFilteredRecords(@PathVariable String searchWord){
+        return recordService.getFilteredRecords(searchWord);
+    }
 }
--- src/main/java/com/g09/webshopspringboot/service/RecordService.java
+++ src/main/java/com/g09/webshopspringboot/service/RecordService.java
@@ -1,7 +1,36 @@
 package com.g09.webshopspringboot.service;
 
+import com.g09.webshopspringboot.domain.Record;
+import com.g09.webshopspringboot.repository.RecordRepository;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class RecordService {
+
+    RecordRepository recordRepository;
+
+    @Autowired
+    public RecordService(RecordRepository recordRepository) {
+        this.recordRepository = recordRepository;
+    }
+
+    public List<Record> getRecords(){
+        return recordRepository.findAll();
+    }
+
+    public Record findById(Long id){
+        return recordRepository.findById(id).orElse(null);
+    }
+
+    public List<Record> getFilteredRecords(String searchWord) {
+        return recordRepository.findAll().stream()
+                .filter(record -> record.getArtist().contains(searchWord) || record.getTitle().contains(searchWord))
+                .collect(Collectors.toList());
+    }
 }
